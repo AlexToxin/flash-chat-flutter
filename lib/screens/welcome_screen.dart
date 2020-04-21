6da3809dcec6a7f64a9c76c6cb98ca825745c666
AlexToxin <avesuslov@gmail.com>
2020-04-21 15:59:35 +0400

    Custom Flutter Animations with the Animation Controller

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
+import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatefulWidget {
@@ -9,11 +10,42 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController animationController;
+  Animation animation;
+  Animation backgroundAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    animationController =
+        AnimationController(duration: Duration(seconds: 2), vsync: this);
+
+    animation =
+        CurvedAnimation(parent: animationController, curve: Curves.bounceIn);
+
+    backgroundAnimation =
+        ColorTween(begin: Colors.lightBlue, end: Colors.grey.shade100)
+            .animate(animationController);
+
+    animationController.forward();
+    animationController.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: backgroundAnimation.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -25,8 +57,11 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 Hero(
                   tag: 'logo',
                   child: Container(
-                    child: Image.asset('images/logo.png'),
-                    height: 60.0,
+                    child: Opacity(
+                        opacity: animation.value,
+                        child: Image.asset('images/logo.png')),
+                    height: 60,
+                    width: 60,
                   ),
                 ),
                 Text(
